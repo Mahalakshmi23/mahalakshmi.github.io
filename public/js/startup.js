64a9722dec2c477196434ef9ff94b2c3d2bd7c6d
prakashn27 <nprakashece@gmail.com>
2015-08-08 20:37:44 -0700

    added console log for command line display ;)

---
+++ public/js/startup.js
@@ -0,0 +1,16 @@
+/* 
+	This file includes all the start up script required for the page
+*/
+var console_write = function() {
+	console.log("Thank you for visiting my Page!!");
+	// console.log(" ");
+	// console.log(" ____  ____     _    _  __   _    ____  _   _ ");
+	// console.log("|  _ \|  _ \   / \  | |/ /  / \  / ___|| | | |");
+	// console.log("| |_) | |_) | / _ \ | ' /  / _ \ \___ \| |_| |");
+	// console.log("|  __/|  _ < / ___ \| . \ / ___ \ ___) |  _  |");
+	// console.log("|_|   |_| \_/_/   \_|_|\_/_/   \_|____/|_| |_|");
+	console.log("My Name is PRAKASH N.");
+	console.log("Please visit http://www.prakashn.com/about/ for more details about me :)")
+	console.log("");
+};
+console_write();
