122291326a67a875e3d894b9f2b6bc3e727f890f
prakashn27 <nprakashece@gmail.com>
2015-08-21 03:10:50 -0400

    VIEW: changed the ASCII art :)

--- public/js/startup.js
+++ public/js/startup.js
@@ -3,14 +3,7 @@
 */
 var console_write = function() {
 	console.log("Thank you for visiting my Page!!");
-	// console.log(" ");
-	// console.log(" ____  ____     _    _  __   _    ____  _   _ ");
-	// console.log("|  _ \|  _ \   / \  | |/ /  / \  / ___|| | | |");
-	// console.log("| |_) | |_) | / _ \ | ' /  / _ \ \___ \| |_| |");
-	// console.log("|  __/|  _ < / ___ \| . \ / ___ \ ___) |  _  |");
-	// console.log("|_|   |_| \_/_/   \_|_|\_/_/   \_|____/|_| |_|");
 	console.log("My Name is PRAKASH N.");
 	console.log("Please visit http://www.prakashn.com/about/ for more details about me :)")
-	console.log("");
 };
 console_write();
